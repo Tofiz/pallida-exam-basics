8bfe57a3532018a50f8f78066bc2aebafdd5d3b2
Tofiz <tofi.zagon@gmail.com>
2017-10-06 12:13:12 +0200

    feat(favorite_animals.py): almost done, the program can't save animals only once, have to fix it later

--- favouriteanimals/favourite_animals.py
+++ favouriteanimals/favourite_animals.py
@@ -7,3 +7,28 @@
 # One animal should be stored only at once
 # Each animal should be written in separate lines
 # The program should only save animals, no need to print them
+
+import sys
+arguments = sys.argv
+
+
+def command():
+
+    if len(arguments) == 1:
+        print("fav_animals [animal] [animal]")
+    elif len(arguments) > 1 :
+        copy_the_arguments_to_file()
+
+
+def copy_the_arguments_to_file():
+    try:
+        with open("favourites.txt", "a+") as f:
+        
+            for i in range(len(arguments)):
+                if arguments[i] not in f:
+                    f.write(str(arguments[i] + "\n"))
+    except Exception:
+        print("File write error!")
+
+
+command()
